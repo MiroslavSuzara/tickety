dac5f4b255294f1de2d0e66a14043e139507a75f
Miroslav Suzara <miroslav.suzara@gmail.com>
2015-02-13 10:10:01 -0800

    Installed devise.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '4.2.0'
 gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+
+gem 'bootstrap-sass'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -31,6 +34,16 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+gem 'devise'
+
+group :development do
+  gem "interactive_editor"
+  gem "awesome_print"
+  gem "hirb"
+  gem "pry-rails"
+end
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -36,6 +37,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  #devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
