9482c5b9728fc1e5cb5e9d6f48423456ada7365f
Miroslav Suzara <miroslav.suzara@gmail.com>
2015-02-13 10:25:05 -0800

    Created tickets controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   
 
-  root "home#index"
+  devise_for :users
+  root "tickets#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
